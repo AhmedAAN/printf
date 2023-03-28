cdb0729e5a66891da1e6391344dfc4f8dad4080c
JAsomugha <jasomugha68@hotmail.com>
2023-03-28 14:45:41 -0700

    get_width

--- get_width.c
+++ get_width.c
@@ -10,31 +10,31 @@
  */
 int get_width(const char *format, int *i, va_list list)
 {
-    int pointer;
-    int totalWidth = 0;
+	int pointer;
+	int totalWidth = 0;
 
-    /* Loop over the characters in the format starting from the current index */
-    for (pointer = *i + 1; format[pointer] != '\0'; pointer++)
-    {
-        /* If the character is a digit, add it to the current width value */
-        if (is_digit(format[pointer]))
-        {
-            totalWidth *= 10;
-            totalWidth += format[pointer] - '0';
-        }
-        /* If the character is a wildcard, get the next argument in the list as the width value */
-        else if (format[pointer] == '*')
-        {
-            pointer++;
-            totalWidth = va_arg(list, int);
-            break;
-        }
-        /* If the character is not a digit or wildcard, stop processing */
-        else
-            break;
-    }
-    /* Update the current index pointer and return the calculated width value */
-    *i = pointer - 1;
+	/* Loop over the characters in the format starting from the current index */
+	for (pointer = *i + 1; format[pointer] != '\0'; pointer++)
+	{
+		/* If the character is a digit, add it to the current width value */
+		if (is_digit(format[pointer]))
+		{
+			totalWidth *= 10;
+			totalWidth += format[pointer] - '0';
+		}
+		/*If the character is a wildcard, get the next argument in the list as the width value*/
+		else if (format[pointer] == '*')
+		{
+			pointer++;
+			totalWidth = va_arg(list, int);
+			break;
+		}
+		/* If the character is not a digit or wildcard, stop processing */
+		else
+			break;
+	}
+	/* Update the current index pointer and return the calculated width value */
+	*i = pointer - 1;
 
-    return (totalWidth);
+	return (totalWidth);
 }
